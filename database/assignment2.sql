2c4da1bd9761e303cb3bed3eb4c812eaa15d3ffb
tympush <tymur.pushnoy@gmail.com>
2025-07-10 18:16:49 +0200

    week 2 sql activity

---
+++ database/assignment2.sql
@@ -0,0 +1,33 @@
+--1
+INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+--2
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_id = 1;
+
+--3
+DELETE FROM public.account
+WHERE account_id = 1;
+
+--4
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
+WHERE inv_id = 25;
+
+--5
+SELECT 
+	inventory.inv_make, 
+    inventory.inv_model, 
+    classification.classification_name
+FROM public.inventory
+INNER JOIN public.classification 
+    ON inventory.classification_id = classification.classification_id
+WHERE classification.classification_name = 'Sport';
+
+--6
+UPDATE public.inventory
+SET 
+	inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
