ee0bf65b9ce3bdbd520f5eeeed9d6f5c1ffbebc0
tympush <tymur.pushnoy@gmail.com>
2025-07-15 13:06:03 +0200

    Inventory Delivery By Classification finished

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 // Index route
 app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
