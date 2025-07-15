8d1efde1b517aaed8c8356083dfad187dc594e2a
tympush <tymur.pushnoy@gmail.com>
2025-07-15 14:24:04 +0200

    updated the styling for inventory items

--- utilities/index.js
+++ utilities/index.js
@@ -40,7 +40,7 @@ Util.buildClassificationGrid = async function(data){
       +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
       +' on CSE Motors" /></a>'
       grid += '<div class="namePrice">'
-      grid += '<hr />'
+      /*grid += '<hr />'*/
       grid += '<h2>'
       grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
       + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
